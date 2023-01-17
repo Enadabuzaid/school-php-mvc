2460c0d7fd50f5b2e9552fff29e105f7622eca0c
Enad Abuzaid <enad.abuzaid15@gmail.com>
2023-01-17 09:23:15 +0300

    add new vide 16 to 20

--- private/views/includes/nav.view.php
+++ private/views/includes/nav.view.php
@@ -1,13 +1,20 @@
 <style>
-	nav ul li a:hover{
-		background-color: grey;
-		color: white !important;
+	nav ul li a{
+		width: 110px;
+    text-align: center;
+    border-left: solid thin #eee;
+    border-right: solid thin #fff;
 	}
+  nav ul li a:hover{
+    background-color: grey;
+    color: white !important;
+  }
+  
 </style>
 <nav class="navbar navbar-expand-lg navbar-light bg-light p-2">
   	<a class="navbar-brand" href="#">
   		<img src="<?=IMAGES_PATH?>/logo.png" class="" style="width:50px;">
-  		My School
+  		 
 	</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -18,7 +25,13 @@
         <a class="nav-link active" href="<?=ROOT?>">DASHBOARD</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="<?=ROOT?>/users">USERS</a>
+        <a class="nav-link" href="<?=ROOT?>/schools">SCHOOLS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?=ROOT?>/users">STAFF</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?=ROOT?>/students">STUDENTS</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="<?=ROOT?>/classes">CLASSES</a>
@@ -27,18 +40,20 @@
       <li class="nav-item">
         <a class="nav-link" href="<?=ROOT?>/tests">TESTS</a>
       </li>
-
+    </ul>
+    <ul class="navbar-nav ms-auto">
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          USER
+          user
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
           <a class="dropdown-item" href="<?=ROOT?>/profile">Profile</a>
           <a class="dropdown-item" href="<?=ROOT?>">Dashboard</a>
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="<?=ROOT?>/logout">Logout</a>
         </div>
       </li>
+
     </ul>
   </div>
 </nav>
